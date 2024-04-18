e739a29d037fc7f851b535c4568c0b1551655189
hbc <bcxxxxxx@gmail.com>
2024-04-17 22:06:18 -0700

    feat: implement az cli auth

--- internal/kusto/auth.go
+++ internal/kusto/auth.go
@@ -10,14 +10,21 @@ func (a AuthOptions) UseClientSecret() bool {
 	return a.TenantID != "" && a.ClientID != "" && a.ClientSecret != ""
 }
 
+func (a AuthOptions) UseAZCLI() bool {
+	return a.AZCLI
+}
+
 func (a AuthOptions) PrepareKustoConnectionStringBuilder(b *kusto.ConnectionStringBuilder) {
-	if a.UseClientSecret() {
+	switch {
+	case a.UseAZCLI():
+		b.WithAzCli()
+	case a.UseClientSecret():
 		b.WithAadAppKey(a.ClientID, a.ClientSecret, a.TenantID)
 	}
 }
 
 func (a AuthOptions) Validate() error {
-	if a.UseClientSecret() {
+	if a.UseClientSecret() || a.UseAZCLI() {
 		return nil
 	}
 
